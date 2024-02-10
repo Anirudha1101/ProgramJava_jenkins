bb534145717487c0711046cd7a394ad063019c4d
Anirudha1101 <156400142+Anirudha1101@users.noreply.github.com>
2024-02-10 22:21:59 +0530

    hello commit

---
+++ Fibo.java
@@ -0,0 +1,21 @@
+
+public class Fibo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =0;
+		int b =1;
+		int c;
+		for(int i=1;i<=10;i++) {
+			
+			c=a+b;
+			System.out.println("fibo sereise="+i +" " +c);
+			a=b;
+			b=c;
+			
+		}
+
+	}
+
+}
